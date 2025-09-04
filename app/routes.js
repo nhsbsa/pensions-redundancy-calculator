674c39dbea482501ea1833537e8ad7b0b53c37d1
Iona Smith <iona.smith3@nhs.net>
2025-09-04 19:13:59 +0100

    membership page added

--- app/routes.js
+++ app/routes.js
@@ -7,7 +7,21 @@ const router = express.Router();
 
 //MVP Routing//
 
-
+module.exports = function (router) {
+  
+    router.post('complexities-list', function (req, res) {
+      const scheme = req.body.scheme;
+  
+      if (scheme === '2015') {
+        // Redirect to your special 2015-only page
+        res.redirect('/complexities-list');
+      } else {
+        // Default page for all other selections
+        res.redirect('/scheme-selection-kickout');
+      }
+    });
+  
+  };
 
 
 module.exports = router;
